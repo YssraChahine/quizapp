5d8bd5166a2c7e97a07862908c1559349f797fe9
Yssra Chahine <chahine@live.de>
2026-02-13 15:23:00 +0100

    counter

--- form.js
+++ form.js
@@ -6,3 +6,9 @@ questionInput.addEventListener("input", (event) =>{
     questionCounter.textContent = 150 - event.target.value.length;
 });
 
+const answerInput = document.querySelector('[data-js="answer"]');
+const answerCounter = document.querySelector('[data-js="answer-counter"]');
+
+answerInput.addEventListener("input", (event) =>{
+    answerCounter.textContent = 150 - event.target.value.length;
+});
