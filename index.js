6d0d22d8fb5488391049c443bd6624277d93d19f
Yssra Chahine <chahine@live.de>
2026-02-13 12:35:22 +0100

    add toggle for first card-show-answer

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const bookmark = document.querySelector('[data-js=bookmark-clicked]')
 bookmark.addEventListener("click", ()=>{
     bookmark.classList.toggle("active");
 })
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const ShowAnswer = document.querySelector('[data-js="answer"]');
+
+answerButton.addEventListener("click", () =>{
+    ShowAnswer.toggleAttribute("hidden");
+    answerButton.textContent = ShowAnswer.hasAttribute("hidden") ? "Show Answer" : "Hide Answer";
+})
